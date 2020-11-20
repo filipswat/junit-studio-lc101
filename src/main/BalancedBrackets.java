5fbb6780b00cd46b0c388954f7ae64949cd6852f
filipswat <31748796+filipswat@users.noreply.github.com>
2020-11-19 23:06:39 -0600

    wrote the code

--- src/main/BalancedBrackets.java
+++ src/main/BalancedBrackets.java
@@ -1,5 +1,5 @@
 package main;
-
+import java.util.ArrayList;
 
 public class BalancedBrackets {
     /**
@@ -22,14 +22,83 @@ public class BalancedBrackets {
      * @return true if balanced, false otherwise
      */
     public static boolean hasBalancedBrackets(String str) {
+        int currPos = 0;
+        int lastPos = 0;
+        String tempString;
+        ArrayList<String> strSegs = new ArrayList<>();
+        ArrayList<Boolean> quoteTrack = new ArrayList<>();
+        char typeOfQuote;
+        int i;
         int brackets = 0;
-        for (char ch : str.toCharArray()) {
-            if (ch == '[') {
-                brackets++;
-            } else if (ch == ']') {
-                brackets--;
+
+        //convert str to character array
+        char[] charArray = str.toCharArray();
+        //for i = 0 to length of character array
+        while (currPos < charArray.length){
+            //if character is single or double quote
+            if ((charArray[currPos] == '\'') || (charArray[currPos] == '\"')){
+                //if lastPos != currPos (indicating something exists before current position)
+                if (lastPos != currPos) {
+                    //place characters from lastPos to previous character in strSegs
+                    //add false to quoteTrack
+                    tempString = new String(charArray, lastPos, currPos - lastPos);
+                    strSegs.add(tempString);
+                    quoteTrack.add(false);
+                    //update lastPos (position of open quote)
+                    lastPos = currPos;
+                }
+                //update typeOfQuote to single or double quote
+                typeOfQuote = charArray[currPos];
+                //do
+                    //increment currentPos
+                    //while current character != typeOfQuote
+                do {
+                    currPos = currPos + 1;
+                } while (charArray[currPos] != typeOfQuote);
+                //place characters from lastPos to currentPos in strSegs
+                //add true to quoteTrack
+                tempString = new String(charArray, lastPos, currPos - lastPos + 1);
+                strSegs.add(tempString);
+                quoteTrack.add(true);
+                //if not at end of array
+                    //lastPos = next character
+                if (currPos != charArray.length - 1) {
+                    lastPos = currPos + 1;
+                } else {
+                    lastPos = currPos;
+                }
+            }
+            //increment currPos
+            currPos++;
+        }
+
+        //make sure that entire string is added to character array
+        if (currPos != lastPos){
+            tempString = new String(charArray, lastPos, currPos - lastPos);
+            strSegs.add(tempString);
+            quoteTrack.add(false);
+        }
+
+        for (i = 0; i < strSegs.size(); i++) {
+            //for i = 0 to size of strSegs
+                //if !quoteTrack[i]
+                //run bracket check
+            if (!quoteTrack.get(i)) {
+                //bracketCheck
+                for (char ch : strSegs.get(i).toCharArray()) {
+                    if (ch == '[') {
+                        brackets++;
+                    } else if (ch == ']') {
+                        brackets--;
+                        if (brackets < 0) {
+                            return false;
+                        }
+                    }
+                }
+                //return brackets == 0;
             }
         }
+
         return brackets == 0;
     }
 }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,89 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
-    //TODO: add tests here
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void emptyString(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
     }
 
+    @Test
+    public void onlyBracketsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
 
+    @Test
+    public void noBracketsNoQuotes(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("word"));
+    }
+
+    @Test
+    public void singleBracketNoQuotes(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void manyBalancedBracketsNoQuotes(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[word] ]][]    [sa]"));
+    }
+
+    @Test
+    public void manyUnbalancedBracketsNoQuotes(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[aa ][]aa [[]aa ]]"));
+    }
+
+    @Test
+    public void bracketsInWrongOrderNoQuotes(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]sdf ddd[[   d]"));
+    }
+
+    @Test
+    public void noBracketsQuotes(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Hello"));
+    }
+
+    @Test
+    public void textWithBalancedBracketsInQuotes(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("hello '[]' world"));
+    }
+
+    @Test
+    public void textWithUnbalancedBracketsInQuotes(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("hello '[[[' world"));
+    }
+
+    @Test
+    public void balancedBracketsAlsoQuotes(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("hello [] 'other text' []"));
+    }
+
+    @Test
+    public void unbalancedBracketsAlsoQuotes(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("hello []] 'other text' []"));
+    }
+
+    @Test
+    public void balancedBracketsUnbalancedBracketsInQuotes(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("hello [] 'other [[[ text' []"));
+    }
+
+    @Test
+    public void unbalancedBracketsBalancingInQuotes(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("hello [[[ ']]]'"));
+    }
+
+    @Test
+    public void balancedBracketsMultipleQuotes(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("hello [[ '[[''[[' ]]"));
+    }
+
+    @Test
+    public void unbalancedBracketsMultipleQuotes(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("hello [[[ '[[['']]' ']]]']"));
+    }
 }
